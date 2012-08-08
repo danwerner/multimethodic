873c8c270e6e1728dcca79f90b34298bc7421469
Daniel Werner <daniel.d.werner@googlemail.com>
2012-08-08 20:21:58 +0200

    Prepare setup.py for 1.0 release

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
-""" setup.py for multimethods-py
+""" setup.py for multimethods
 """
 
 from distutils.core import setup
 
 setup(
     name         = 'multimethods',
-    version      = '0.1',
+    version      = '1.0',
     description  = 'Clojure-style multimethods for Python',
     author       = 'Daniel Werner',
+    author_email = 'daniel.d.werner@googlemail.com',
+    license      = 'BSD 2-clause',
+    keywords     = 'multimethods dispatch',
+    classifiers  = [
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
     url          = 'http://github.com/danwerner/multimethods',
     py_modules   = ['multimethods'],
-    entry_points = {},
 )
